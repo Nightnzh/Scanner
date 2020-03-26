0183c2e25ecb5e2e3d51be48cb21bca44d6c6d2d
013476 <night.nzh@gmail.com>
2020-03-26 16:53:51 +0800

    326

--- app/src/main/java/com/night/scanner/ui/home/HomeFragment.java
+++ app/src/main/java/com/night/scanner/ui/home/HomeFragment.java
@@ -1,10 +1,13 @@
 package com.night.scanner.ui.home;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
@@ -27,20 +30,35 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        radapter = new Radapter(requireContext());
-        final RecyclerView recycler = root.findViewById(R.id.recycler);
+
+        final View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        radapter = new Radapter(requireContext(),requireActivity().getApplication());
+        RecyclerView recycler = root.findViewById(R.id.recycler);
         Button buttonScan = root.findViewById(R.id.button_scan);
         recycler.setHasFixedSize(true);
         recycler.setLayoutManager(new  LinearLayoutManager(requireContext()));
         recycler.setAdapter(radapter);
+
         homeViewModel.getAllQrCodes().observe(getViewLifecycleOwner(), new Observer<List<QrCode>>() {
             @Override
             public void onChanged(List<QrCode> qrCodes) {
                 radapter.setQrCodes(qrCodes);
             }
         });
-
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO::
+            }
+        });
         return root;
+    }
+    private class Mtask extends AsyncTask<Void,Void,Void>{
+        @Override
+        protected Void doInBackground(Void... voids) {
+            homeViewModel.insert(new QrCode("fafa"));
+            return null;
+        }
     }
 }
